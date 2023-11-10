44fcf7cf5f400125a85ef548ceaaaec6f59f7167
Erick22 <52467300+erick22js@users.noreply.github.com>
2023-11-10 11:14:59 -0300

    NativeTest - Its not Java, but used eclipse on it

---
+++ NativeTest/src/NativeTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : NativeTest.cpp
+// Author      : Erick
+// Version     :
+// Copyright   : bone, anyone can use if want
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
